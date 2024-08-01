e3d7e3be4ea8bea53bf73890a5026aca04b3b30e
math198512 <thoraichi@gmail.com>
2024-08-01 19:37:08 +0100

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module that contains a single function which takes
+a float n as argument and returns the floor of the float.
+"""
+
+
+def floor(n: float) -> int:
+    """ returns the floor of the float. """
+    return int(n)
