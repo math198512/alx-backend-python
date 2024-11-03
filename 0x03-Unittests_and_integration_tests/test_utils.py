183c3f57c195de52d6f396ef98f3f125512dcc12
math198512 <thoraichi@gmail.com>
2024-11-03 15:11:32 +0100

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,4 +22,5 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map_exception(self, nested_map, path):
         """method to test test_access_nested_map"""
-        self.assertRaises(access_nested_map(nested_map, path))
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
