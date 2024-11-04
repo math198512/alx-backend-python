8a09f794e597e7f3f849c7434210ee67be0b4cd3
math198512 <thoraichi@gmail.com>
2024-11-04 13:36:09 +0100

    test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,22 +2,23 @@
 """Testings for the Client module"""
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import MagicMock, patch, Mock
 from client import GithubOrgClient
+from typing import Dict
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """to test utils.get_json"""
     @parameterized.expand([
-        ("google"),
-        ("abc")
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
     ])
     @patch('client.get_json')
-    def test_org(self, data, mock):
-        """
-        self descriptive
-        """
-        endpoint = 'https://api.github.com/orgs/{}'.format(data)
-        spec = GithubOrgClient(data)
-        spec.org()
-        mock.assert_called_once_with(endpoint)
+    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        """Tests the `org` method."""
+        mocked_fxn.return_value = MagicMock(return_value=resp)
+        gh_org_client = GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), resp)
+        mocked_fxn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
